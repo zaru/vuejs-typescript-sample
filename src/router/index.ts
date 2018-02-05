053e1ef15e5b253204578a1d9582491a0c7b31df
zaru <sakuraba@basicinc.jp>
2018-02-05 13:16:48 +0900

    Added vue ext

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import TodoList from '@/components/TodoList'
+import HelloWorld from '@/components/HelloWorld.vue'
+import TodoList from '@/components/TodoList.vue'
 
 Vue.use(Router)
 
